7e4f2108a01dc65f4aeb770d095d1805958b700a
Arpan Sen <sen.arpan12@gmail.com>
2024-07-30 03:12:01 +0530

    Added collaborative rooms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,23 @@
 import Header from "@/components/Header";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
 import { Button } from "@/components/ui/button";
+import { dateConverter } from "@/lib/utils";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
   const documents = [];
+
   return (
     <main className="home-container">
       <Header className="sticky left-0 top-0">
-        <div className="flex items-centergap-2 lg:gap-4">
-          Notification
+        <div className="flex items-center gap-2 lg:gap-4">
           <SignedIn>
             <UserButton />
           </SignedIn>
@@ -27,6 +34,11 @@ const Home = () => {
             width={40}
             height={40}
             className="mx-auto"
+          />
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
           />
         </div>
       )}
